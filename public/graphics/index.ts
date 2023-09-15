dd12731c85ec56588962b8c747692af2087da983
Catapa <catalin24pap@gmail.com>
2023-09-16 01:12:17 +0300

    implement footer

--- public/graphics/index.ts
+++ public/graphics/index.ts
@@ -3,11 +3,13 @@ import Logo from './logo_4x.png';
 import AboutImg from './about_4x.jpg';
 import AboutBlob from './About_blob.svg';
 import Triangle from './triangle_3x.png';
+import UpcraftyLogo from './upcrafty_logo_4x.png';
 
 export {
 	HeroBG,
 	Logo,
 	AboutImg,
 	AboutBlob,
-	Triangle
+	Triangle,
+	UpcraftyLogo
 };
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,8 +1,27 @@
+import Image from "next/image";
+import { Logo, Triangle, UpcraftyLogo } from "../../../public/graphics";
+import Link from "next/link";
+import './Footer.css';
+
 const Footer = () => {
 	return (
-		<header>
-			Footer
-		</header>
+		<footer className={'relative grid grid-cols-3 items-center justify-items-center bg-cream px-5 pt-4 pb-12'}>
+			<Image src={Triangle} alt={''} className={'absolute w-[70px] left-1/2 -translate-x-1/2 -translate-y-2/3'}/>
+			<Image src={Logo} alt={'knots & bells'} className={'w-[188px] justify-self-start'}/>
+			<div className={'flex flex-row items-center h-full mt-10'}>
+				<span className={'text-black font-bold text-base'}>Made with ❤️ by</span>
+				<Link href={'htp://www.upcrafty.com'}>
+					<Image src={UpcraftyLogo} alt={'Upcrafty - Crafting digital experiences'} className={'w-[60px] ml-2 translate-y-[1px]'}/>
+				</Link>
+			</div>
+			<nav className={'space-x-12 justify-self-end'}>
+				<Link href={'#'} className={'navbar-link'}>Vendors</Link>
+				<Link href={'#'} className={'navbar-link'}>About Us</Link>
+				<Link href={'#'} className={'navbar-link'}>Blog</Link>
+				<Link href={'#'} className={'navbar-link'}>FAQ</Link>
+				<Link href={'#'} className={'navbar-link'}>Contact Us</Link>
+			</nav>
+		</footer>
 	);
 };
 export default Footer;
